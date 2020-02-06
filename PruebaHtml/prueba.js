73799a2fd79ddd9a75c4a92fc709d563f5a3d171
Alvaro garcia <alvarogarcia16102000@gmail.com>
2020-02-06 10:48:18 +0100

    Borrado de archivos basura

--- PruebaHtml/prueba.js
+++ PruebaHtml/prueba.js
@@ -1,52 +1,11 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
+//Servidor con nodejs y express
+
+const express = require('express')
+const app = express()
 const port = 3000
 
-//const Gpio = require('onoff').Gpio
-//const pinLED = new Gpio(4, 'out')
+app.use(express.static(__dirname + '/public/'))
 
-const server = http.createServer(function(req, res){
-    /*Para poder leer distintos tipos de archivos*/
-    var filePath = '.' + req.url
-    if (filePath == './')
-        filePath = './index.html'
+app.get('/', (req, res) => res.sendFile('index.html'))
 
-    //console.log("Serving " + filePath)
-
-    const ext = path.extname(filePath)
-    var contentType = 'application/octet-stream'
-
-    switch (ext) {
-        case '.css':
-            contentType = 'text/css'
-            break
-        case '.html':
-            contentType = 'text/html'
-            break
-        case '.js':
-            contentType = 'text/javascript'
-            break
-    }
-
-    /*Lee el html*/
-    fs.readFile(filePath, function(err, data){
-        if(err){
-            res.writeHead(404)
-            res.write("Error: File Not Found")
-        } else{
-            res.writeHead(200, { 'Content-Type' : contentType })
-            res.write(data)
-        }
-        res.end()
-    })
-})
-
-server.listen(port, function(err){
-    if(err){
-        console.log("Something went wrong", err)
-    } else{
-        console.log("Server is listening")
-    }
-})
-
+app.listen(port, () => console.log(`Server listen on port ${port}`))

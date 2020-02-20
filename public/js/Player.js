b7ce63e2edc82fb1dcf2f3adb50bc7bb91238e5f
envilk <envilkc@gmail.com>
2020-02-20 12:12:05 +0100

    CodeMirror executes everything

--- public/js/Player.js
+++ public/js/Player.js
@@ -1,7 +1,16 @@
 //Remake without animations
 
 class Player extends Phaser.GameObjects.Sprite {
-    constructor() {
-
+    constructor(scene) {
+        super(scene);
+        this.scene = scene;
+    }
+    
+    printG(){
+        console.log('G');
+    }
+    
+    printF(){
+        console.log('F');
     }
 }

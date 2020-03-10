aa75cbf9d934942cb04b4593c6fc0a87b75132c1
Alvaro garcia <alvarogarcia16102000@gmail.com>
2020-03-10 11:37:12 +0100

    Check if raspi is connected

--- raspi.js
+++ raspi.js
@@ -1,18 +1,41 @@
-const Gpio = require('onoff');
-let LED = {
-    value: 0
-};//new Gpio();
+const isPi = require('detect-rpi');
+
+let valLED = 0;
+let valBUT = 0;
+
+if(isPi()){ 
+    const Gpio = require('onoff').Gpio;
+    const LEDR = new Gpio(4, 'out');
+    const BUTR = new Gpio(22, 'out', 'rising');
+
+    BUTR.watch(function (err, value){
+        if(err){
+            console.error(err);
+            return;
+        }
+        valBUT = !valBUT;   //When push the button change valBUT
+    });
+
+    function unexportOnClose() {
+        LEDR.writeSync(0);
+        LEDR.unexport();
+        BUTR.unexport();
+        process.exit();
+    }
+
+    process.on('SIGINT', unexportOnClose);  //When press ctrl+c 
+}
 
 const raspiWrite = (component, value) => {
-    if(component === 'LED'){
-        LED.value = value;
-        console.log(LED.value);
-    }
+    if(component === 'LED')
+        isPi() ? LEDR.writeSync(value) : valLED = value;
 };
 
 const raspiRead = (component) => {
     if(component === 'LED')
-        return LED.value; 
+        return isPi() ? LEDR.readSync() : valLED;
+    else if (component === 'BUT')
+        return valBUT;
 };
 
 module.exports = {raspiWrite, raspiRead};

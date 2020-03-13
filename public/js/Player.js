0352b0287a2a1324d2395964e658743bc7385b67
Alvaro garcia <alvarogarcia16102000@gmail.com>
2020-03-14 00:09:32 +0100

    Camera debug mode

--- public/js/Player.js
+++ public/js/Player.js
@@ -53,82 +53,89 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     moveRight(numberOfMovs) {
         //16 is the distance between the center of two tiles, or the distance the sprite has to travel
         //to reach the next tile
-        while(this.isMoving);
-        this.isMoving = true;
-        if (this.canMove) {
-            this.target.x = this.x + 32 * numberOfMovs;
-            this.target.y = this.y;
-
-            this.direction = 'right';
-            this.animationName = "walk-up";
-            this.setFlipX(false);
-            this.startAnimation();
-
-            //30 means that the sprite goes as fast as 30pixels per second (Is the value of this.body.speed)
-            this.scene.physics.moveToObject(this, this.target, 30);
-        } else if(!numberOfMovs) console.error("Funcion vacia");
+        if(isNaN(numberOfMovs)) console.error("Funcion vacia");
+        else{
+            while(this.isMoving) console.log("waiting...");
+            this.isMoving = true;
+            if (this.canMove) {
+                this.target.x = this.x + 32 * numberOfMovs;
+                this.target.y = this.y;
+    
+                this.direction = 'right';
+                this.animationName = "walk-up";
+                this.setFlipX(false);
+                this.startAnimation();
+    
+                //30 means that the sprite goes as fast as 30pixels per second (Is the value of this.body.speed)
+                this.scene.physics.moveToObject(this, this.target, 30);
+            } 
+        }
     }
 
     moveLeft(numberOfMovs) {
         //16 is the distance between the center of two tiles, or the distance the sprite has to travel
         //to reach the next tile
-        while(this.isMoving);
-        this.isMoving = true;
-        if (this.canMove) {
-            while(this.isMoving){}
-            this.target.x = this.x - 32 * numberOfMovs;
-            this.target.y = this.y;
-
-            this.direction = 'left';
-            this.animationName = "walk-up";
-            this.setFlipX(true);
-            this.startAnimation();
-
-            //30 means that the sprite goes as fast as 30pixels per second (Is the value of this.body.speed)
-            this.scene.physics.moveToObject(this, this.target, 30);
+        if(!numberOfMovs) console.error("Funcion vacia");
+        else{
+            while(this.isMoving);
+            this.isMoving = true;
+            if (this.canMove) {
+                while(this.isMoving){}
+                this.target.x = this.x - 32 * numberOfMovs;
+                this.target.y = this.y;
+    
+                this.direction = 'left';
+                this.animationName = "walk-up";
+                this.setFlipX(true);
+                this.startAnimation();
+    
+                //30 means that the sprite goes as fast as 30pixels per second (Is the value of this.body.speed)
+                this.scene.physics.moveToObject(this, this.target, 30);
+            }
         }
     }
 
     moveUp(numberOfMovs) {
         //16 is the distance between the center of two tiles, or the distance the sprite has to travel
         //to reach the next tile
-        while(this.isMoving);
-        this.isMoving = true;
-        if (this.canMove) {
-            this.target.x = this.x;
-            //y coordinate is "reversed", that is: positive y means DOWN and negative y means UP
-            this.target.y = this.y - 32 * numberOfMovs;
-
-            this.direction = 'up';
-            this.animationName = 'walk-up';
-            this.startAnimation();
-
-            //30 means that the sprite goes as fast as 30pixels per second (Is the value of this.body.speed)
-            this.scene.physics.moveToObject(this, this.target, 30);
+        if(!numberOfMovs) console.error("Funcion vacia");
+        else{
+            while(this.isMoving);
+            this.isMoving = true;
+            if (this.canMove) {
+                this.target.x = this.x;
+                //y coordinate is "reversed", that is: positive y means DOWN and negative y means UP
+                this.target.y = this.y - 32 * numberOfMovs;
+    
+                this.direction = 'up';
+                this.animationName = 'walk-up';
+                this.startAnimation();
+    
+                //30 means that the sprite goes as fast as 30pixels per second (Is the value of this.body.speed)
+                this.scene.physics.moveToObject(this, this.target, 30);
+            }
         }
     }
 
     moveDown(numberOfMovs) {
         //16 is the distance between the center of two tiles, or the distance the sprite has to travel
         //to reach the next tile
-        while(this.isMoving);
-        this.isMoving = true;
-        if (this.canMove) {
-            this.target.x = this.x;
-            this.target.y = this.y + 32 * numberOfMovs;
-
-            this.direction = 'down';
-            this.animationName = 'walk-up';
-            this.startAnimation();
-
-            //30 means that the sprite goes as fast as 30pixels per second (Is the value of this.body.speed)
-            this.scene.physics.moveToObject(this, this.target, 30);
+        if(!numberOfMovs) console.error("Funcion vacia");
+        else {
+            while(this.isMoving);
+            this.isMoving = true;
+            if (this.canMove) {
+                this.target.x = this.x;
+                this.target.y = this.y + 32 * numberOfMovs;
+    
+                this.direction = 'down';
+                this.animationName = 'walk-up';
+                this.startAnimation();
+    
+                //30 means that the sprite goes as fast as 30pixels per second (Is the value of this.body.speed)
+                this.scene.physics.moveToObject(this, this.target, 30);
+            }
         }
-    }
-
-    moveCamera(){
-        this.sceneB = this.scene.get('SceneDown');
-        this.sceneB.moveCamera();
     }
 
     //An example of use raspiClient
@@ -154,11 +161,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         this.anims.stop();
     }
 
-    /**
-     * Called before Update.
-     * @param {object} time
-     * @param {number} delta
-     */
+    //Before scene update
     preUpdate(time, delta) {
         super.preUpdate(time, delta);
 
@@ -166,6 +169,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         if (this.x === this.target.x && this.y === this.target.y) {
             this.stopAnimation();
             this.isMoving = false;
+            console.log("Ha llegado");
         }
 
         if (this.canMove) {

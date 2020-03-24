47e6b2a237ed63e5f46eea537279635584bbf385
Alvaro garcia <alvarogarcia16102000@gmail.com>
2020-03-24 18:27:27 +0100

    Add enrique zombies and system of levels

--- public/js/phaser.js
+++ public/js/phaser.js
@@ -2,7 +2,7 @@ const widthD = document.getElementById('gameContainer').clientWidth
 const heightD = document.getElementById('gameContainer').clientHeight
 
 const config = {
-    type: Phaser.AUTO,  //Automatically select between Canvas and Webgl
+    type: Phaser.CANVAS,  //Automatically select between Canvas and Webgl
     width: widthD,
     height: heightD,
     parent: 'gameContainer',
@@ -11,7 +11,7 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            debug: false
+            debug: true
         }
     }
 }

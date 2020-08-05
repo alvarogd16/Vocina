67e4be57b4251438e3dda4766d95c30129fc82cc
envilk <envilkc@gmail.com>
2020-08-05 20:53:53 +0200

    menu theme added

--- public/js/phaserMenu.js
+++ public/js/phaserMenu.js
@@ -8,6 +8,9 @@ class Menu extends Phaser.Scene {
 
 	preload(){
 		//Load images files
+                this.load.audio('menuTheme', [
+            'assets/sounds/menuTheme.mp3'
+        ])
 
 		this.load.image("maskImg", "assets/menu/mask.png");
 		this.load.image("textImg", "assets/menu/text.png");
@@ -20,6 +23,24 @@ class Menu extends Phaser.Scene {
 		//Add images to the screen
 		this.add.image(0, 0, "maskImg").setOrigin(0);
 		this.add.image(0, 0, "textImg").setOrigin(0);
+        
+        //Play menu theme
+        var loopMarker = {
+            name: 'loop',
+            config: {
+                loop: true
+            }
+        };
+        
+        this.menuTheme = this.sound.add('menuTheme');
+        //this.menuTheme.play();
+        
+        this.menuTheme.addMarker(loopMarker);
+
+        // Delay option can only be passed in config
+        this.menuTheme.play('loop', {
+            delay: 0
+        });
 	}
 }
 

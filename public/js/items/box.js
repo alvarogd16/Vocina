e11e7cf8852e4241cb81172940ce493ce6563375
envilk <envilkc@gmail.com>
2020-09-03 11:02:51 +0200

    box class done [sublevels system incomplete]

--- public/js/items/box.js
+++ public/js/items/box.js
@@ -8,12 +8,18 @@ class Box extends Phaser.Physics.Arcade.Sprite {
         this.correctBox = correctBox;
     }
     
-    probar(boxNumber) {
+    probar(boxCode) {
         if(this.correctBox && boxCode == 45) {
-            //Playing sink sound
+            //Playing unlocked box sound
             this.unlockedBox = this.scene.sound.add('unlockedBox');
             this.unlockedBox.play();
-            console.log("CAJA"+ this.correctBox+" - codigo correcto: "+boxCode);
+            console.log(" - codigo correcto: "+boxCode);
         }
+        else if(!this.correctBox && boxCode == 999){
+            //Playing locked box sound
+            this.lockedBox = this.scene.sound.add('lockedBox');
+            this.lockedBox.play();
+            console.log(" - ningún código resultó útil");        
+            }
     }
 }
